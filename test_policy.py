9b6956ed781553691f0c89f5a73e2b386dc6a452
khsoukaina <150720175+khsoukaina@users.noreply.github.com>
2024-03-27 22:34:21 +0000

    test_policy.py

--- test_policy.py
+++ test_policy.py
@@ -2,10 +2,10 @@ from gridworld import GridWorld
 from policy import policy
 import matplotlib.pyplot as plt
 
-# Créez une instance de Grid World
+# Créer une instance de Grid World
 mdp_instance = GridWorld(noise=0.2, width=3, height=3, initial_state=(0, 0), goals={(2, 2): 1}, blocked_states=[(1, 1)], action_cost=-0.04)
 
-# Créez une instance de votre politique avec l'instance Grid World comme argument
+# Créer une instance de votre politique avec l'instance Grid World comme argument
 policy_instance = policy({} ,mdp_instance)
 
 state = mdp_instance.get_initial_state()
@@ -13,15 +13,16 @@ plt.ion()  # Active le mode interactif de Matplotlib
 
 while not mdp_instance.is_terminal(state):
     # Affiche la grille avec la position actuelle de l'agent
-    plt.clf()  # Nettoyez la figure courante pour préparer la nouvelle visualisation
+    plt.clf()  # Nettoie la figure courante pour préparer la nouvelle visualisation
     mdp_instance.visualise(agent_position=state)  # Visualisez l'état actuel avec la position de l'agent
-    plt.draw()  # Mettez à jour la fenêtre graphique avec le contenu dessiné
+    plt.draw()  # Mise à jour de la fenêtre graphique avec le contenu dessiné
     plt.pause(0.1)  # Une courte pause pour s'assurer que la fenêtre graphique se met à jour
 
     input("Appuyez sur Entrée pour faire avancer l'agent...")  # Attendre que l'utilisateur appuie sur Entrée
     action = policy_instance.select_action(state)
     new_state, reward = mdp_instance.execute(state, action)
     
+    # Mise à jour de l'état pour la prochaine itération
     state = new_state
 
 plt.ioff()  # Désactive le mode interactif
